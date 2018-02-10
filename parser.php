1a99057d1605226317282b82d2203c224b80dc15
David Dejmal <dejmal.d@gmail.com>
2018-02-10 20:52:48 +0100

    pokus o nacteni slova

--- parser.php
+++ parser.php
@@ -4,16 +4,29 @@
 $param_help_text="Ahoj jsem paramtert help!"; //TODO
 // TODO exit nebo return ???
 
+
 if( $argv[1] == "--help")
 {
 	if($argc == 2)
 	{
 		print ($param_help_text);
-		exit (15);
+		exit (0);
 	}
-	exit (1); 	//TODO
+ 	else 
+	{
+		exit (10); 	
+	}
 }
-
+	$token="";
+	print("ctu");
+	$char=fgetc(STDIN);
+	while(($char!=PHP_EOL) || ($char!=' '));
+	{
+		$token=$token+$char;
+		$char=fgetc(STDIN);	
+	}
+	print($token);
+	exit(0);
 ?>
 
 

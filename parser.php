9a6e8a98837fe5d34aab529ad912695bd81e6922
DejmalDavid <dejmal.d@gmail.com>
2018-02-14 23:41:20 +0100

    get_token v2
    otestovane vraci tokeny se spravnym poradim

--- parser.php
+++ parser.php
@@ -1,37 +1,39 @@
 <?php
 
-//Zpraovani paramatru
+
 $param_help_text="Ahoj jsem paramtert help!"; //TODO
 // TODO exit nebo return ???
 
-$STDIN = fopen("input.txt", "r");
+//Globalni promene 
+$STDIN = fopen("input2.txt", "r");
 $citac_poradi=0;
+$nuluj_citac=FALSE;
 
+//simulace struktury tokeny
 class Tokeny{
     public $text;
     public $poradi;
 }
 
-/*
-if( $argv[1] == "--help")
+//Zpraovani paramatru
+if($argc == 2)
 {
-	if($argc == 2)
+	if( $argv[1] == "--help")
 	{
 		print ($param_help_text);
 		exit (0);
 	}
- 	else 
-	{
-		exit (10); 	
-	}
-}*/
+}
+elseif ($argc > 2) {
+    exit(42);
+}
 
 
 
-
+// Vraci string tokenu a nastavuje globalni promenou citac_poradi
  function Get_token()
         {
-            global $STDIN,$citac_poradi;
+            global $STDIN,$citac_poradi,$nuluj_citac;
             $znak=fgetc($STDIN);
             $char=ord($znak);
             while ($znak !== FALSE)
@@ -73,29 +75,72 @@ if( $argv[1] == "--help")
                     $znak=fgetc($STDIN);
                     $char=ord($znak); 
                 }
-                
+                if($nuluj_citac== TRUE)
+                {
+                    $nuluj_citac=FAlSE;
+                    $citac_poradi=0;
+                }
                 if(($char == 10)|| ($char == 13)) 
                 {
-                   $citac_poradi=0;
+                   $nuluj_citac=TRUE;
                 }
-                
+                $citac_poradi++;
                 return $token;
             }
             return "KONEC";
         }
   
     
-        for(  $i=0 ; $i< 12; $i++)
+       for(  $i=0 ; $i< 50; $i++)
         {
             $slovo = new Tokeny();
             $slovo->text = Get_token();
             $slovo->poradi = $citac_poradi;
-            $citac_poradi++;
+            
    
 
             printf("%s p:%d\n",$slovo->text,$slovo->poradi);       
         }
+       
+        /*
+            $slovo1 = new Tokeny();
+            $slovo1->text = Get_token();
+            $slovo1->poradi = $citac_poradi;
+            printf("%s p:%d\n",$slovo1->text,$slovo1->poradi); 
         
+                        $slovo9 = new Tokeny();
+            $slovo9->text = Get_token();
+            $slovo9->poradi = $citac_poradi;
+            printf("%s p:%d\n",$slovo9->text,$slovo9->poradi);
+                        $slovo2 = new Tokeny();
+            $slovo2->text = Get_token();
+            $slovo2->poradi = $citac_poradi;
+            printf("%s p:%d\n",$slovo2->text,$slovo2->poradi);
+                        $slovo3 = new Tokeny();
+            $slovo3->text = Get_token();
+            $slovo3->poradi = $citac_poradi;
+            printf("%s p:%d\n",$slovo3->text,$slovo3->poradi);
+                        $slovo4 = new Tokeny();
+            $slovo4->text = Get_token();
+            $slovo4->poradi = $citac_poradi;
+            printf("%s p:%d\n",$slovo4->text,$slovo4->poradi);
+                        $slovo5 = new Tokeny();
+            $slovo5->text = Get_token();
+            $slovo5->poradi = $citac_poradi;
+            printf("%s p:%d\n",$slovo5->text,$slovo5->poradi);
+                        $slovo6 = new Tokeny();
+            $slovo6->text = Get_token();
+            $slovo6->poradi = $citac_poradi;
+            printf("%s p:%d\n",$slovo6->text,$slovo6->poradi);
+                                 $slovo7 = new Tokeny();
+            $slovo7->text = Get_token();
+            $slovo7->poradi = $citac_poradi;
+            printf("%s p:%d\n",$slovo7->text,$slovo7->poradi);
+                                             $slovo8 = new Tokeny();
+            $slovo8->text = Get_token();
+            $slovo8->poradi = $citac_poradi;
+            printf("%s p:%d\n",$slovo8->text,$slovo8->poradi);
+        */
         //TODO pokayde volat ord($char) a podminka konce je FALSE === $char
         /*
     $char=fgetc($STDIN);
